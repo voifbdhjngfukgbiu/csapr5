b4f402f6dd21f8f251ac487c1e3cef1d5a97b40a
voifbdhjngfukgbiu <zaha.ftbl24@gmail.com>
2025-03-30 11:19:19 +0300

    Initial commit

---
+++ library-game/Program.cs
@@ -0,0 +1,22 @@
+﻿string filePath = "GameLibrary.txt";
+Console.Write("СКолько игр хотите добавить: ");
+int count = int.Parse(Console.ReadLine());
+for (int i =0; i < count;i++){
+    Console.Write("Введите жанр: ");
+    string genre = Console.ReadLine();
+    Console.Write("Название игры: ");
+    string game = Console.ReadLine();
+    Console.Write("Название студии разработи: ");
+    string studio = Console.ReadLine();
+    string gameInfo = $"Игра  {genre} : {{Название: {game}, Разработчик: {studio}}} ";
+    File.AppendAllText(filePath, gameInfo + "\n"); 
+}
+Console.WriteLine("Игры добавлены в библиотеку");
+if (File.Exists(filePath)){
+    string[] lines = File.ReadAllLines(filePath); 
+    foreach (string line in lines)
+    {
+    Console.WriteLine(line);
+    }
+}
+
